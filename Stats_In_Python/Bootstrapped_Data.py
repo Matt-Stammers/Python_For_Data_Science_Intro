4560ab2892668afc083f36db8fcf1ab4694450ec
Matt Stammers <matthewstammers@googlemail.com>
2017-07-27 18:08:03 +0100

    Update Bootstrapped_Data.py

--- Stats_In_Python/Bootstrapped_Data.py
+++ Stats_In_Python/Bootstrapped_Data.py
@@ -68,3 +68,73 @@ plt.show()
 np.percentile(bs_replicates, [2.5, 97.5])
 
 # This shows how powerful bootstrapped data can be in validating results and computing confidence intervals.
+
+# You can also use it to plot the variance:
+
+# Generate 10,000 bootstrap replicates of the variance: bs_replicates
+bs_replicates = draw_bs_reps(admissions, np.var, size = 10000)
+
+# Put the variance in units of single patients rather than hundreds as it was in the initial data
+bs_replicates /= 100
+
+# Make a histogram of the results
+_ = plt.hist(bs_replicates, normed = True, bins = 50)
+_ = plt.xlabel('variance of admissions (numbers)')
+_ = plt.ylabel('PDF')
+
+# Show the plot
+plt.show()
+
+# You can also bootstrap data in pairs to use it for regression problems by using np.polyfit as well.
+
+def draw_bs_pairs_linreg(x, y, size=1):
+    """Perform pairs bootstrap for linear regression."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_slope_reps, bs_intercept_reps
+    bs_slope_reps = np.empty(size)
+    bs_intercept_reps = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, size=len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x, bs_y, 1)
+
+    return bs_slope_reps, bs_intercept_reps
+
+# for the histogram:
+
+# Generate replicates of slope and intercept using pairs bootstrap
+bs_slope_reps, bs_intercept_reps = draw_bs_pairs_linreg(
+                    admission_rate, death, size=1000)
+
+# Compute and print 95% CI for slope
+print(np.percentile(bs_slope_reps, [2.5, 97.5]))
+
+# Plot the histogram
+_ = plt.hist(bs_slope_reps, bins=50, normed=True)
+_ = plt.xlabel('slope')
+_ = plt.ylabel('PDF')
+plt.show()
+
+# for linear regression:
+
+# Generate array of x-values for bootstrap lines: x
+x = np.array([0, 100])
+
+# Plot the bootstrap lines
+for i in range(100):
+    _ = plt.plot(x, bs_slope_reps[i] * x + bs_intercept_reps[i],
+                 linewidth=0.5, alpha=0.2, color='red')
+
+# Plot the data
+_ = plt.plot(admission_rate, death, marker='.', linestyle='none')
+
+# Label axes, set the margins, and show the plot
+_ = plt.xlabel('admission rate')
+_ = plt.ylabel('death %')
+plt.margins(0.02)
+plt.show()
